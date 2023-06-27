575f207737afe4dc787edc923933ecce250214f0
Banuka Ambegoda <89307136+BanukaKA@users.noreply.github.com>
2023-06-27 14:25:49 -0400

    Update App.vue

--- FootballClient-VueNPMproject/src/App.vue
+++ FootballClient-VueNPMproject/src/App.vue
@@ -1,8 +1,27 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+   <nav class="navbar navbar-expand-sm navbar-toggleable-sm navbar-dark bg-dark border-bottom box-shadow mb-3">
+            <div class="container-fluid">
+                
+                <a class="navbar-brand"><img src="https://www.pngmart.com/files/22/NFL-PNG-File.png" height="40" width="40" /></a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target=".navbar-collapse" aria-controls="navbarSupportedContent"
+                        aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="navbar-collapse collapse d-sm-inline-flex justify-content-end">
+                    <ul class="navbar-nav flex-grow-1">
+                        <li class="nav-item">
+                            <router-link class="nav-link" to="/home"><b>Home</b></router-link>
+                        </li>
+                        <li class="nav-item">
+                            <router-link class="nav-link" to="/team"><b>Team</b></router-link>
+                        </li>
+                        <li class="nav-item">
+                            <router-link class="nav-link" to="/player"><b>Player</b></router-link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
   <router-view/>
 </template>
 
